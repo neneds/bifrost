c2b748ec17457cf752114a27bb0fb4aabd451527
Dennis <dennis.rodrigues@aisdigital.com.br>
2019-03-08 13:36:08 -0300

    Initial Commit

---
+++ Bifrost/Bifrost.h
@@ -0,0 +1,19 @@
+//
+//  Bifrost.h
+//  Bifrost
+//
+//  Created by Dennis Merli on 08/03/19.
+//  Copyright © 2019 Dennis Merli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bifrost.
+FOUNDATION_EXPORT double BifrostVersionNumber;
+
+//! Project version string for Bifrost.
+FOUNDATION_EXPORT const unsigned char BifrostVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bifrost/PublicHeader.h>
+
+
